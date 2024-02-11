533186a21bea9a0f87dfc95013e193167f2133e8
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-02-11 20:29:44 +0100

    "registro 200% terminado"

--- ecommerce-next/src/components/Auth/RegisterForm/RegisterForm.js
+++ ecommerce-next/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,8 +1,13 @@
 import { Form } from "semantic-ui-react";
 import { useFormik } from "formik";
+import { useRouter } from "next/router";
+import { Auth } from "@/api";
 import { initialValues, validationSchema } from "./RegisterForm.form";
 
+const authCtrl = new Auth();
+
 export function RegisterForm() {
+  const router = useRouter();
 
   const formik = useFormik({
     initialValues: initialValues(),
@@ -19,50 +24,48 @@ export function RegisterForm() {
   });
 
   return (
-
     <Form onSubmit={formik.handleSubmit}>
-    <Form.Group widths="equal">
-      <Form.Input
-        name="email"
-        type="text"
-        placeholder="Correo electronico"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        error={formik.errors.email}
-      />
-      <Form.Input
-        name="username"
-        type="text"
-        placeholder="Nombre de usuario"
-        value={formik.values.username}
-        onChange={formik.handleChange}
-        error={formik.errors.username}
-      />
-    </Form.Group>
+      <Form.Group widths="equal">
+        <Form.Input
+          name="email"
+          type="text"
+          placeholder="Correo electronico"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+        />
+        <Form.Input
+          name="username"
+          type="text"
+          placeholder="Nombre de usuario"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.errors.username}
+        />
+      </Form.Group>
 
-    <Form.Group widths="equal">
-      <Form.Input
-        name="name"
-        type="text"
-        placeholder="Nombre y apellidos"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        error={formik.errors.name}
-      />
-      <Form.Input
-        name="password"
-        type="password"
-        placeholder="Contraseña"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        error={formik.errors.password}
-      />
-    </Form.Group>
+      <Form.Group widths="equal">
+        <Form.Input
+          name="name"
+          type="text"
+          placeholder="Nombre y apellidos"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.errors.name}
+        />
+        <Form.Input
+          name="password"
+          type="password"
+          placeholder="Contraseña"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+        />
+      </Form.Group>
 
-    <Form.Button type="submit" fluid loading={formik.isSubmitting}>
-      Registrarse
-    </Form.Button>
-  </Form>
-    
+      <Form.Button type="submit" fluid loading={formik.isSubmitting}>
+        Registrarse
+      </Form.Button>
+    </Form>
   );
 }
