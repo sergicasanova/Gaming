a3cd2b2a978ff940ff84e878795be5efa396de1a
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-02-17 15:52:47 +0100

    "Buscador he implementacion de resultados de busqueda implementados"

--- ecommerce-next/src/pages/search/search.js
+++ ecommerce-next/src/pages/search/search.js
@@ -1,18 +1,45 @@
-import {BasicLayout} from "@/layouts"
 import { useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import { size } from "lodash";
+import { BasicLayout } from "@/layouts";
+import {
+  GridGames,
+  NoResult,
+  Pagination,
+  Separator,
+} from "@/components/Shared";
 
-
-export default function search() {
+export default function SearchPage(props) {
+  const { games, pagination, searchText } = props;
+  const hasResult = size(games) > 0;
 
   useEffect(() => {
     document.getElementById("search-games").focus();
   }, []);
-  
+
   return (
     <>
       <BasicLayout relative isOpenSearch>
-        <h2>Estamos en la busqueda</h2>
+        <Container>
+          <Separator height={50} />
+
+          <h2>Buscando: {searchText}</h2>
+          {hasResult ? (
+            <>
+              <GridGames games={games} />
+              <Separator height={30} />
+              <Pagination
+                currentPage={pagination.page}
+                totalPages={pagination.pageCount}
+              />
+            </>
+          ) : (
+            <NoResult text="No se han encontrado resultados" />
+          )}
+
+          <Separator height={100} />
+        </Container>
       </BasicLayout>
     </>
-  )
+  );
 }
