e383d243c0ba09e6a94582650f2e36ccc68d84f1
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-02-18 22:04:21 +0100

    "empezamos carrito"

--- ecommerce-next/src/pages/cart/cart.js
+++ ecommerce-next/src/pages/cart/cart.js
@@ -1,12 +1,22 @@
 import { CartLayout } from "@/layouts";
+import { useRouter } from "next/router";
 
 
 
 export default function CartPage() {
+    const {
+    query: { step = 1 },
+  } = useRouter();
+  const currentStep = Number(step);
+  const [games, setGames] = useState(null);
+
+
   return (
     <>
         <CartLayout>
-            <h2>Carrito</h2>
+            {currentStep === 1 && <p>Step ONE</p>}
+            {currentStep === 2 && <p>Step Dos</p>}
+            {currentStep === 3 && <p>Step Tres</p>}
         </CartLayout>
     </>
   )
